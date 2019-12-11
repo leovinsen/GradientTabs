4843d5586843a1a4eea33dcb04342feb6c367223
Leonardo Vinsen <leonardovinsen@gmail.com>
2019-12-11 22:16:50 +0700

    Migrated app to AndroidX in order to create the library module

--- app/src/main/java/com/example/gradienttabs/MainActivity.java
+++ app/src/main/java/com/example/gradienttabs/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.gradienttabs;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.FrameLayout;
 
